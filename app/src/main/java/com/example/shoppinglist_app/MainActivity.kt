14f8be341d8466cf5ae8a7ab6ad35da18c5938a6
issam-abida <issam.abida10@gmail.com>
2024-08-23 00:49:45 +0200

    Adding logs to main

--- app/src/main/java/com/example/shoppinglist_app/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist_app/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.shoppinglist_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "Issam Logs"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //Here is my First modig
+        Log.d(TAG, "Debug Logs starts from here")
+
     }
 }
