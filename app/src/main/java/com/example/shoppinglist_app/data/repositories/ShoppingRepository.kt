bb1c4d60e7a91e905666b420ba6b64d37eda5b00
issam-abida <issam.abida10@gmail.com>
2024-08-25 10:04:44 +0200

    Adding database repository

---
+++ app/src/main/java/com/example/shoppinglist_app/data/repositories/ShoppingRepository.kt
@@ -0,0 +1,16 @@
+package com.example.shoppinglist_app.data.repositories
+
+import com.example.shoppinglist_app.data.db.ShoppingDatabase
+import com.example.shoppinglist_app.data.db.entities.ShoppingItem
+
+class ShoppingRepository(private val db: ShoppingDatabase){
+    suspend fun insert(item: ShoppingItem){
+        db.getShoppingDao().insert(item)
+    }
+
+    suspend fun delete(item: ShoppingItem){
+        db.getShoppingDao().delete(item)
+    }
+
+    fun getAllShoppingItems() = db.getShoppingDao().getAllShoppingItems()
+}
